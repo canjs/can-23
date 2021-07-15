352c343a445d164f51a850d0132e14da84857155
Justin Meyer <justinbmeyer@gmail.com>
2021-07-14 22:20:06 -0500

    wrong path

--- control/control.js
+++ control/control.js
@@ -7,7 +7,7 @@
 // ## helpers
 
 var Construct = require("can-construct");
-require("./construct/proxy/proxy");
+require("../construct/proxy/proxy");
 var can = require("../can-core");
 var assign = require("can-assign");
 var observeReader = require("can-stache-key");
